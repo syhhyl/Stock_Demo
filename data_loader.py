1cb3a4351bab7d04745bd69e5839f5972de2e294
syhhyl <syhhyl926@126.com>
2025-09-07 18:36:47 +0800

    revert failed

--- data_loader.py
+++ data_loader.py
@@ -3,13 +3,24 @@ import pandas as pd
 import numpy as np
 
 def fetch_stock_data(stock_code):
-    df = ak.stock_zh_a_hist(symbol=stock_code, start_date="20150101", period="daily", adjust="qfq")
-    df = df[['日期', '开盘', '最高', '最低', '收盘', '成交量']]
-    df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.sort_values('date').reset_index(drop=True)
-    return df
+  '''
+  fetch_stock_data get a stock infomation include:
+  date open high low close volume 
+  '''
+  df = ak.stock_zh_a_hist(symbol=stock_code, start_date="20150101", period="daily", adjust="qfq")
+  df = df[['日期', '开盘', '最高', '最低', '收盘', '成交量']]
+  df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+  df['date'] = pd.to_datetime(df['date'])
+  df = df.sort_values('date').reset_index(drop=True)
+  return df
 
+def save_stock_data(stock_code):
+  try:
+    df = fetch_stock_data(stock_code)
+    df.to_pickle(f'{stock_code}.pkl')
+  except:
+    print("save fail")
+  
 
 def compute_kdj(df, n=9):
     low_min = df['low'].rolling(window=n).min()
@@ -24,34 +35,12 @@ def compute_kdj(df, n=9):
     print(df['J'])
     return df
 
-def feature_engineering(df):
-    # df['return'] = df['close'].pct_change()
-    # df['ma5'] = df['close'].rolling(5).mean()
-    # df['ma10'] = df['close'].rolling(10).mean()
-    # df['ma_diff'] = df['ma5'] - df['ma10']
-    # df['vol_ratio'] = df['volume'] / df['volume'].rolling(5).mean()
-    # df['high_close_diff'] = df['high'] - df['close']
-    # df['close_low_diff'] = df['close'] - df['low']
-    # df['range'] = df['high'] - df['low']
-    # df['return_sign'] = np.sign(df['return'])
-    df['label'] = (df['close'].shift(-2) > df['close']).astype(int)  # 改进标签定义
-
-    df = compute_kdj(df)
-    df = df.dropna().reset_index(drop=True)
-    return df
-
-def prepare_data_for_model(df):
-    # features = [
-    #     'return', 'ma_diff', 'vol_ratio', 'high_close_diff',
-    #     'close_low_diff', 'range', 'return_sign', 'J', 'kdj_buy_signal'
-    # ]
-    features = ['J']
-    X = df[features]
-    y = df['label']
-    return X, y
-
 if __name__ == "__main__":
   df = fetch_stock_data("600030")
-  df = compute_kdj(df)
+  df.to_pickle("stock_data.pkl")
   print(df)
+  print("read data.pkl")
+  df = pd.read_pickle("stock_data.pkl")
+  print(df)
+  # print(df.iloc[20])
   
